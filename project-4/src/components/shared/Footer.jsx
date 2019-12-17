8852e95d95c549ffe0c0235396e97ac28a6dcc21
Chris Abrams <abramsc32@gmail.com>
2019-12-17 10:34:35 -0500

    added shared folder with shared styles folder

---
+++ project-4/src/components/shared/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/footer.css'
+
+function Footer() {
+    return (
+        <>
+         <h2>IN A PINCH</h2>
+        </>
+    )
+}
+
+export default Footer
