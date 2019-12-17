ca8281e4531a5073984f0875bf48a4879e884be7
Andrew Chang <andychang.92@gmail.com>
2019-12-17 18:47:04 -0500

    added onhover to cards

--- project-4/src/components/shared/Card.jsx
+++ project-4/src/components/shared/Card.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import './styles/card.css'
+import React from "react";
+import "./styles/card.css";
 
+const Card = props => {
+  return (
+    <div className="main-card-cont">
+      <div className="main-card">
+        <img src={props.image} />
+        <div className="quickView">QuickView</div>
+        <div>{props.recipeName}</div>
+        <div>Prep Time: {props.prepTime}</div>
+      </div>
+    </div>
+  );
+};
 
-
-
-const Card = (props) => {
-        return (
-            <div className="main-card-cont">
-            <div className="main-card">
-            <img src={props.image} />
-            <div>{props.recipeName}</div>
-            <div>Prep Time: {props.prepTime}</div>
-            </div>
-            </div>
-        )
-}
-    
-export default Card
+export default Card;
