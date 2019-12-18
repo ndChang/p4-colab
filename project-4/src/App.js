b7d7fce9d1c4259c0bf5f7a014e54967072685ae
Melissa Donegan <melissadonegan25@gmail.com>
2019-12-18 16:01:31 -0500

    added css

--- project-4/src/App.js
+++ project-4/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from './components/Container'
+import Footer from './components/shared/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Container />
+      <Footer /> 
     </div>
   );
 }
--- project-4/src/components/shared/Nav.jsx
+++ project-4/src/components/shared/Nav.jsx
@@ -4,13 +4,16 @@ function Nav() {
     return (
         <>
             <div className='nav'>
-            <div className='cart-counter'></div>
+            <div className='line-bar'>
+                <img src="https://i.imgur.com/2f0hWvM.png" alt="line-bar" />
+            </div>
                 <div className='site-title'>
                     <h1>IN A PINCH</h1>
                 </div>
             <div className='nav-container'>
                 <img className='icon-1' src='https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-512.png' alt='user picture' />
                 <p>ABOUT</p>
+                <div className='cart-counter'></div>
                 <img className='icon-2' src='https://www.searchpng.com/wp-content/uploads/2019/02/Shopping-Cart-PNG-Icon-715x715.png' alt='shopping cart' />
                 </div>
             </div>
